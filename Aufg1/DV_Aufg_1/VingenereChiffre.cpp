8fc58bafdb385455d4a7c9616c0ff48d4a614bc1
AgeInPee <henrik.peters.98@web.de>
2018-04-27 14:49:20 +0200

    io working

--- Aufg1/DV_Aufg_1/VingenereChiffre.cpp
+++ Aufg1/DV_Aufg_1/VingenereChiffre.cpp
@@ -1,11 +1,61 @@
-#include "VingenereChiffre.h"
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
 
+std::string encrypt() {
 
-VingenereChiffre::VingenereChiffre()
-{
+	return "e";
 }
 
-
-VingenereChiffre::~VingenereChiffre()
-{
+std::string decrypt() {
+	return "v";
 }
+
+int main() {
+	char cmdin = '0';
+	std::string infile = "";
+	std::string key = "Hamburg";
+	std::string VingenereQuad[26] = {
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+		"BCDEFGHIJKLMNOPQRSTUVWXYZA",
+		"CDEFGHIJKLMNOPQRSTUVWXYZAB",
+		"DEFGHIJKLMNOPQRSTUVWXYZABC",
+		"EFGHIJKLMNOPQRSTUVWXYZABCD",
+		"FGHIJKLMNOPQRSTUVWXYZABCDE",
+		"GHIJKLMNOPQRSTUVWXYZABCDEF",
+		"HIJKLMNOPQRSTUVWXYZABCDEFG",
+		"IJKLMNOPQRSTUVWXYZABCDEFGH",
+		"JKLMNOPQRSTUVWXYZABCDEFGHI",
+		"KLMNOPQRSTUVWXYZABCDEFGHIJ",
+		"LMNOPQRSTUVWXYZABCDEFGHIJK",
+		"MNOPQRSTUVWXYZABCDEFGHIJKL",
+		"NOPQRSTUVWXYZABCDEFGHIJKLM",
+		"OPQRSTUVWXYZABCDEFGHIJKLMN",
+		"PQRSTUVWXYZABCDEFGHIJKLMNO",
+		"QRSTUVWXYZABCDEFGHIJKLMNOP",
+		"RSTUVWXYZABCDEFGHIJKLMNOPQ",
+		"STUVWXYZABCDEFGHIJKLMNOPQR",
+		"TUVWXYZABCDEFGHIJKLMNOPQRS",
+		"UVWXYZABCDEFGHIJKLMNOPQRST",
+		"VWXYZABCDEFGHIJKLMNOPQRSTU",
+		"WXYZABCDEFGHIJKLMNOPQRSTUV",
+		"XYZABCDEFGHIJKLMNOPQRSTUVW",
+		"YZABCDEFGHIJKLMNOPQRSTUVWX",
+		"ZABCDEFGHIJKLMNOPQRSTUVWXY" };
+	
+	while (cmdin != 'q') {
+		std::cout << "Druecke entweder v, e oder q." << std::endl;
+		std::cin >> cmdin;
+		if (cmdin == 'e') {
+			//lese Datei und führe encrypt auf ihr aus
+			std::cout << encrypt() << std::endl;
+		}
+		else if (cmdin == 'v') {
+			//lese Datei und führe decrypt auf ihr aus
+			std::cout << decrypt() << std::endl;
+		};
+	};
+	return 0;
+}
+
