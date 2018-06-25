a0e4ee9a1f1d490c8ffa7135ae4878f8cae05485
AgeInPee <henrik.peters.98@web.de>
2018-06-25 21:40:35 +0200

    some progress

--- Aufg3/Aufg3/Main.cpp
+++ Aufg3/Aufg3/Main.cpp
@@ -3,14 +3,19 @@
 //#include <glad/glad.h>
 //#include <GLFW/glfw3.h>
 #include <string>
+#include <vector>
 #include <list>
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
-#include <stb_image_write.h>
-//#include <glm/glm.hpp>
+//#include <stb_image_write.h>
+#include <glm/glm.hpp>
 
 std::list<int> task3_1(int c);
 void print_task3_1(std::list<int> sol);
-void test3_2(int x, int y);
+void task3_2(int x, int y);
+std::vector<int> gen_img(int x, int y);
+glm::dvec2 gen_vec(int xpos, int ypos, int xres, int yres);
 
 int main()
 {
@@ -18,7 +23,9 @@ int main()
 	int num = 0;
 
 	while (cmdin != 'q') {
-		std::cout << "Druecke q zum beenden. Druecke a Aufg 1 zu starten." << std::endl;
+		std::cout << "Druecke q zum beenden." << std::endl;
+		std::cout << "Druecke a um Aufg 1 zu starten." << std::endl;
+		std::cout << "Druecke b um Aufg 2 zu starten." << std::endl;
 		std::cin >> cmdin;
 		if (cmdin == 'a') {
 			//lese Datei und führe encrypt auf ihr aus
@@ -27,14 +34,50 @@ int main()
 			std::cin >> num;
 			print_task3_1(task3_1(num));
 			std::cout << "-----------------------------------------------" << std::endl;
+		} else if (cmdin == 'b') {
+			std::cout << "-----------------------------------------------" << std::endl;
+			task3_2(100, 100);
 		}
 	}
 	return 0;
 }
 
-void test3_2(int x, int y) 
+void task3_2(int xres, int yres) 
 {
+	std::vector<int> img = gen_img(xres, yres);
 	
+	for (std::vector<int>::iterator it = img.begin(); it != img.end(); it++) {
+		int xpos = std::distance(img.begin(), it) % xres;
+		int ypos = std::distance(img.begin(), it) / yres;
+		std::cout << xpos << ", " << ypos << std::endl;
+		std::cout << gen_vec(xpos, ypos, xres, yres)[0] << ", " << gen_vec(xpos, ypos, xres, yres)[1] << std::endl;
+	}
+}
+
+glm::dvec2 gen_vec(int xpos, int ypos, int xres, int yres)
+{
+	glm::dvec2 v1 = glm::dvec2(-(ypos - (2 / 8) * yres), xpos - (1 / 5) * xres);
+	glm::dvec2 v2 = glm::dvec2(ypos - (5 / 8) * yres, -(xpos - (7 / 10)*xres));
+	return ( ( (v1) / (glm::abs(v1)*glm::abs(v1)) ) + ( (v2) / (glm::abs(v2)*glm::abs(v2)) ) ) / 
+			glm::abs( ( (v1) / (glm::abs(v1)*glm::abs(v1)) ) + ( (v2) / (glm::abs(v2)*glm::abs(v2)) ) );
+	// IND means floating point exceeded
+	// check formula --> alternitives to glm?
+}
+
+
+std::vector<int> gen_img(int x, int y)
+{
+	std::vector<int> img;
+	img.assign(x*y, 0);
+
+	std::srand(std::time(0));
+	std::cout << x << ", " << y << ", " << img.size() << std::endl;
+
+	for (std::vector<int>::iterator it = img.begin(); it != img.end(); it++) {
+		int random = std::rand() % 2;
+		*it = random;
+	}
+	return img;
 }
 
 std::list<int> task3_1(int c) 
